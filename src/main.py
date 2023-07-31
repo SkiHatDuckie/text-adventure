bc9c408d1a6f4aae6bfe7e812ea76972a21f81a4
ConeRobo <skihatduckie@gmail.com>
2023-07-31 13:01:03 -0400

    Fix window not closing properly

--- src/main.py
+++ src/main.py
@@ -16,29 +16,24 @@ def main(width, height, fps):
     }
     active_scene = scene_ids[0]()
 
-    while True:
+    quit_attempt = False
+    while not quit_attempt:
         pressed_keys = py.key.get_pressed()
         pressed_mouse = py.mouse.get_pressed(3)
 
         # Event filtering
         filtered_events = []
         for event in py.event.get():
-            quit_attempt = False
-
             if event.type == py.QUIT:
                 quit_attempt = True
             elif event.type == py.KEYDOWN:
-                alt_pressed = pressed_keys[py.K_LALT] or \
-                              pressed_keys[py.K_RALT]
-                if event.key == py.K_ESCAPE:
+                alt_pressed = pressed_keys[py.K_LALT] \
+                                or pressed_keys[py.K_RALT]
+                if event.key == py.K_ESCAPE \
+                    or event.key == py.K_F4 and alt_pressed:
                     quit_attempt = True
-                elif event.key == py.K_F4 and alt_pressed:
-                    quit_attempt = True
-
-            if quit_attempt:
-                active_scene.terminate()
-                break
-            else:
+            
+            if not quit_attempt:
                 filtered_events.append(event)
 
         active_scene.processInput(filtered_events, pressed_keys, pressed_mouse)
@@ -47,6 +42,9 @@ def main(width, height, fps):
 
         py.display.flip()
         clock.tick(fps)
+
+    # Cleanup
+    active_scene.terminate()
 
 
 if __name__ == "__main__":
