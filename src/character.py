4fcbb09c188414474da8ad7ae50e72f420e24029
ConeRobo <skihatduckie@gmail.com>
2023-08-02 15:15:34 -0400

    Add `character.py`

---
+++ src/character.py
@@ -0,0 +1,25 @@
+class Character:
+    def __init__(self, name: str, health: int, level: int, moveset: tuple) -> None:
+        self.name = name
+        self.health = health
+        self.level = level
+        self.moveset = moveset
+
+
+def on_move_claws(user: Character, opp: Character):
+    pass
+
+def on_move_bite(user: Character, opp: Character):
+    pass
+
+def on_move_tail_whip(user: Character, opp: Character):
+    pass
+
+def on_move_savor(user: Character):
+    pass
+
+def on_move_spicy_breath(user: Character, opp: Character):
+    pass
+
+def on_move_flour_throw(user: Character, opp: Character):
+    pass
